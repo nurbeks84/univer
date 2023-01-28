ebf5a0912cd8591a4391a86f39ca0fb7db5cd67a
nurbek84 <nurbekkojjmberdiev84@gmail.com>
2023-01-28 13:15:36 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student("Ermek",23);
+        Student student1 = new Student("Eraly",20);
+        Student student2 = new Student("Elmirbek",18);
+        System.out.println(student);
+        System.out.println(student1);
+        System.out.println(student2);
+    }
+}
