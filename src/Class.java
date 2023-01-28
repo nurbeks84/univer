257a83d5fdae0321d8e58b99050eaee8b9df8785
nurbek84 <nurbekkojjmberdiev84@gmail.com>
2023-01-28 14:35:24 +0600

    ema

---
+++ src/Class.java
@@ -0,0 +1,48 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Class {
+    int number;
+    String soz;
+    int[] mass;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getSoz() {
+        return soz;
+    }
+
+    public void setSoz(String soz) {
+        this.soz = soz;
+    }
+
+    public int[] getMass() {
+        return mass;
+    }
+
+    public void setMass(int[] mass) {
+        this.mass = mass;
+    }
+
+    public Class(int number, String soz, int[] mass) {
+        this.number = number;
+        this.soz = soz;
+        this.mass = mass;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Class{" +
+                "number=" + number +
+                ", soz='" + soz + '\'' +
+                ", mass=" + Arrays.toString(mass) +
+                '}';
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Student student = new Student("Ermek",23);
+    /*    Student student = new Student("Ermek",23);
         Student student1 = new Student("Eraly",20);
         Student student2 = new Student("Elmirbek",18);
         System.out.println(student);
         System.out.println(student1);
-        System.out.println(student2);
+        System.out.println(student2); */
+        Class a = new Class(10,"mektep",new int[]{5,76,23,90});
+        System.out.println(a);
     }
 }
